50efa995545bd4dece21d3bf909f2ea8731eb0e6
Ninnoss <ninos.dinkha97@gmail.com>
2025-01-07 14:11:32 +0300

    added routes - lesson-2 done

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') // this is the path to the controller: api/users
+export class UsersController {
+  @Get() // GET /users or GET /users?role=value
+  findAll(@Query('role') role?: 'admin' | 'intern' | 'engineer') {
+    return role ? { role } : [];
+  }
+
+  @Get('interns') // GET /users/interns
+  findAllInterns() {
+    return [];
+  }
+
+  // the most specific route has to be used last. Any route that comes after it, the segment after /users will be treated as the path parameter
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id: id };
+  }
+
+  @Post() // POST /users
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  updateOne(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  deleteOne(@Param('id') id: string) {
+    return { id };
+  }
+}
