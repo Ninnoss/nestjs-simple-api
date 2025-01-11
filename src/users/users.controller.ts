d25ee7fbabadd0f8f9325ae0f43b308c57a4d8db
Ninnoss <ninos.dinkha97@gmail.com>
2025-01-11 21:45:48 +0300

    add users service logic to handle routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,12 +8,16 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { Role, User, UsersService } from './users.service';
 
 @Controller('users') // this is the path to the controller: api/users
 export class UsersController {
+  // we use injectibale to inject the service into the controller. NestJS will automatically inject the service into the controller and make it available to the methods. If the class is used elsewhere it will pull it from there avoiding creating the class twice
+  constructor(private readonly usersService: UsersService) {}
+
   @Get() // GET /users or GET /users?role=value
-  findAll(@Query('role') role?: 'admin' | 'intern' | 'engineer') {
-    return role ? { role } : [];
+  findAll(@Query('role') role?: Role) {
+    return this.usersService.findAll(role);
   }
 
   @Get('interns') // GET /users/interns
@@ -24,21 +28,21 @@ export class UsersController {
   // the most specific route has to be used last. Any route that comes after it, the segment after /users will be treated as the path parameter
   @Get(':id') // GET /users/:id
   findOne(@Param('id') id: string) {
-    return { id: id };
+    return this.usersService.findOne(+id);
   }
 
   @Post() // POST /users
-  create(@Body() user: {}) {
-    return user;
+  create(@Body() user: User) {
+    return this.usersService.create(user);
   }
 
   @Patch(':id') // PATCH /users/:id
-  updateOne(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
+  updateOne(@Param('id') id: string, @Body() userUpdate: User) {
+    return this.usersService.updateOne(+id, userUpdate);
   }
 
   @Delete(':id') // DELETE /users/:id
   deleteOne(@Param('id') id: string) {
-    return { id };
+    return this.usersService.deleteOne(+id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,91 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  role: Role;
+};
+
+export type Role = 'admin' | 'intern' | 'engineer';
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  // this is a private property in the class.
+  private users = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      email: 'Sincere@april.biz',
+      role: 'intern',
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      email: 'Shanna@melissa.tv',
+      role: 'intern',
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      email: 'Nathan@yesenia.net',
+      role: 'engineer',
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      email: 'Julianne.OConner@kory.org',
+      role: 'engineer',
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      email: 'Lucio_Hettinger@annie.ca',
+      role: 'admin',
+    },
+  ];
+
+  // create the methods based on the controller functions
+  findAll(role?: Role) {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  create(user: User) {
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+    const newUser = {
+      id: usersByHighestId[0].id + 1,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  updateOne(id: number, updatedUser: User) {
+    // const user = this.users.find((user) => user.id === +id);
+    // if (!user) {
+    //   return 'User not found';
+    // }
+
+    this.users = this.users.map((user) => {
+      if (user.id === +id) {
+        return { ...user, ...updatedUser };
+      }
+      return user;
+    });
+
+    return this.findOne(id); // return the updated user
+  }
+
+  deleteOne(id: number) {
+    const removedUser = this.findOne(id);
+    this.users = this.users.filter((user) => user.id !== +id);
+    return removedUser;
+  }
+}
