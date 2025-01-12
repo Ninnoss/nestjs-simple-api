70ebbeb4b152a47d8300b57f45eac3d118dd4f56
Ninnoss <ninos.dinkha97@gmail.com>
2025-01-12 22:48:01 +0300

    add db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { DatabseModule } from './databse/databse.module';
+import { DatabaseService } from './databse/database.service';
+import { DatabaseService } from './databse/database.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DatabseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabaseService],
 })
 export class AppModule {}
