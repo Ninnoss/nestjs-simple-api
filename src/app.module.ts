2aeec43ea5dff9cbb40f00327c7f1c07a72255b4
Ninnoss <ninos.dinkha97@gmail.com>
2025-01-12 23:29:08 +0300

    fix import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { DatabseModule } from './databse/database.module';
-import { DatabaseService } from './databse/database.service';
 import { EmployeesModule } from './employees/employees.module';
+import { DatabaseModule } from './databse/database.module';
 import { DatabaseService } from './databse/database.service';
 
 @Module({
-  imports: [UsersModule, DatabseModule, EmployeesModule],
+  imports: [UsersModule, DatabaseModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService, DatabaseService],
 })
